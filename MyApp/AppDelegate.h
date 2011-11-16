36f93506aa7b80ed3d625a8508262a08e1d3d324
Steven Holloway <impurist@gmail.com>
2011-11-16 14:21:27 +1100

    Initial Commit

---
+++ MyApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyApp
+//
+//  Created by Steven Holloway on 16/11/11.
+//  Copyright (c) 2011 Agile Impetus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyApp
+//
+//  Created by Steven Holloway on 16/11/11.
+//  Copyright (c) 2011 Agile Impetus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
